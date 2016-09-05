6ac3205c80aeddf0f05a068b70dae03de182709f
Kevin Frans <kevin@bobthechicken.com>
2016-09-04 17:23:19 -0700

    init

---
+++ main.py
@@ -0,0 +1,123 @@
+import tensorflow as tf
+import numpy as np
+from ops import *
+import input_data
+from scipy.misc import imsave as ims
+
+
+class Draw():
+    def __init__(self):
+        self.mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+        self.n_samples = self.mnist.train.num_examples
+
+        self.img_size = 28
+        self.read_n = 5
+        self.n_hidden = 256
+        self.n_z = 10
+        self.sequence_length = 10
+        self.batch_size = 64
+        self.share_parameters = False
+
+        self.images = tf.placeholder(tf.float32, [None, 784])
+        self.e = tf.random_normal((self.batch_size, self.n_z), mean=0, stddev=1) # Qsampler noise
+        self.lstm_enc = tf.nn.rnn_cell.LSTMCell(self.n_hidden, state_is_tuple=True) # encoder Op
+        self.lstm_dec = tf.nn.rnn_cell.LSTMCell(self.n_hidden, state_is_tuple=True) # decoder Op
+
+        self.cs = [0] * self.sequence_length
+        self.mu, self.logsigma, self.sigma = [0] * self.sequence_length, [0] * self.sequence_length, [0] * self.sequence_length
+
+        h_dec_prev = tf.zeros((self.batch_size, self.n_hidden))
+        enc_state = self.lstm_enc.zero_state(self.batch_size, tf.float32)
+        dec_state = self.lstm_dec.zero_state(self.batch_size, tf.float32)
+
+        x = self.images
+        for t in range(self.sequence_length):
+            # error image + original image
+            c_prev = tf.zeros((self.batch_size, self.img_size**2)) if t == 0 else self.cs[t-1]
+            x_hat = x - tf.sigmoid(c_prev)
+            # read the image
+            r = self.read_basic(x,x_hat,h_dec_prev)
+            # encode it to guass distrib
+            self.mu[t], self.logsigma[t], self.sigma[t], enc_state = self.encode(enc_state, tf.concat(1, [r, h_dec_prev]))
+            # sample from the distrib to get z
+            z = self.sampleQ(self.mu[t],self.sigma[t])
+            # decode z into image
+            decoded_image_portion, h_dec, dec_state = self.decode(dec_state, z)
+            # write image
+            self.cs[t] = c_prev + self.write_basic(decoded_image_portion) # store results
+            h_dec_prev = h_dec
+            self.share_parameters = True # from now on, share variables
+
+        # the final timestep
+        self.generated_images = tf.nn.sigmoid(self.cs[-1])
+
+        self.generation_loss = tf.reduce_mean(-tf.reduce_sum(self.images * tf.log(1e-10 + self.generated_images) + (1-self.images) * tf.log(1e-10 + 1 - self.generated_images),1))
+
+        kl_terms = [0]*self.sequence_length
+        for t in xrange(self.sequence_length):
+            mu2 = tf.square(self.mu[t])
+            sigma2 = tf.square(self.sigma[t])
+            logsigma = self.logsigma[t]
+            kl_terms[t] = 0.5 * tf.reduce_sum(mu2 + sigma2 - 2*logsigma, 1) - self.sequence_length*0.5
+        self.latent_loss = tf.reduce_mean(tf.add_n(kl_terms))
+        self.cost = self.generation_loss + self.latent_loss
+        optimizer = tf.train.AdamOptimizer(1e-3, beta1=0.5)
+        grads = optimizer.compute_gradients(self.cost)
+        for i,(g,v) in enumerate(grads):
+            if g is not None:
+                grads[i] = (tf.clip_by_norm(g,5),v)
+        self.train_op = optimizer.apply_gradients(grads)
+
+        self.sess = tf.Session()
+        self.sess.run(tf.initialize_all_variables())
+
+    def train(self):
+        for i in xrange(10000):
+            xtrain, _ = self.mnist.train.next_batch(self.batch_size)
+            cs, gen_loss, lat_loss, _ = self.sess.run([self.cs, self.generation_loss, self.latent_loss, self.train_op], feed_dict={self.images: xtrain})
+            if i % 100 == 0:
+                print "iter %d genloss %f latloss %f" % (i, gen_loss, lat_loss)
+                results = np.transpose(np.array(cs),[0,1,2])[0]
+                results_square = np.reshape(results, [-1, 28, 28])
+                print results_square.shape
+                ims("results/"+str(i)+".jpg",merge(results_square,[8,8]))
+
+
+    # the read() operation without attention
+    def read_basic(self, x, x_hat, h_dec_prev):
+        return tf.concat(1,[x,x_hat])
+
+    # encode an attention patch
+    def encode(self, prev_state, image):
+        # update the RNN with image
+        with tf.variable_scope("encoder",reuse=self.share_parameters):
+            hidden_layer, next_state = self.lstm_enc(image, prev_state)
+
+        # map the RNN hidden state to latent variables
+        with tf.variable_scope("mu", reuse=self.share_parameters):
+            mu = dense(hidden_layer, self.n_hidden, self.n_z)
+        with tf.variable_scope("sigma", reuse=self.share_parameters):
+            logsigma = dense(hidden_layer, self.n_hidden, self.n_z)
+            sigma = tf.exp(logsigma)
+        return mu, logsigma, sigma, next_state
+
+
+    def sampleQ(self, mu, sigma):
+        return mu + sigma*self.e
+
+    def decode(self, prev_state, latent):
+        # update decoder RNN with latent var
+        with tf.variable_scope("decoder", reuse=self.share_parameters):
+            hidden_layer, next_state = self.lstm_dec(latent, prev_state)
+
+        # map RNN hidden state to image
+        with tf.variable_scope("write", reuse=self.share_parameters):
+            decoded_image_portion = dense(hidden_layer, self.n_hidden, self.img_size**2)
+        return decoded_image_portion, hidden_layer, next_state
+
+    def write_basic(self, decoded_image_portion):
+        return decoded_image_portion
+
+
+model = Draw()
+model.train()
